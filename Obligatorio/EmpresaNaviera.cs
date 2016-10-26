0577d120ceb219b82873349c754f105e05667fb6
Maxi Aloise <maxialoise@gmail.com>
2016-10-25 23:34:20 -0300

    Make EmpresaNaviera singleton

---
+++ Obligatorio/EmpresaNaviera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class EmpresaNaviera
+    {
+        private static EmpresaNaviera instancia = null;
+        
+
+        private EmpresaNaviera()
+        {
+            //inicializar alguna variable??
+        }
+
+        public static EmpresaNaviera GetInstance()
+        {
+            if (EmpresaNaviera.instancia == null)
+            {
+                EmpresaNaviera.instancia = new EmpresaNaviera();
+            }
+            return EmpresaNaviera.instancia;
+        }
+
+
+    }
+}
