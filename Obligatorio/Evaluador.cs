17c39d01eae5a3218b0d7ce58fe4132fa80bd874
francisco arriola <franarrio@gmail.com>
2017-04-30 20:31:44 -0300

    ,

--- Obligatorio/Evaluador.cs
+++ Obligatorio/Evaluador.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace Dominio
     public class Evaluador
     {
         #region Propiedades
+
+        public int Id { get; set; }
 
         public string Telefono { get; set; }
 
@@ -20,6 +24,56 @@ namespace Dominio
         public Evaluador()
         {
         }
+        #endregion
+
+        #region Metodos
+
+        public static List<Evaluador> ObtenerEvaluadores()
+        {
+            List<Evaluador> lstEvaluadores = null;
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(@"Server=PC-102717\FARRIOLA; Database = Emprendimientos;Integrated Security=SSPI"))
+                {
+                    SqlCommand cmd = new SqlCommand("Obtener_Evaluadores", cnn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cnn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        lstEvaluadores = new List<Evaluador>();
+
+                        while (reader.Read())
+                        {
+                            Evaluador ev = new Evaluador();
+
+                            if (reader["Id"] != DBNull.Value)
+                                ev.Id = (int)reader["Id"];
+
+                            if (reader["Telefono"] != DBNull.Value)
+                                ev.Telefono = (string)reader["Telefono"];
+
+                            if (reader["Calificacion"] != DBNull.Value)
+                                ev.Calificacion = (int)reader["Calificacion"];
+
+                            lstEvaluadores.Add(ev);
+                        }
+                    }
+
+                    cmd.Dispose();
+                }
+
+                return lstEvaluadores;
+            }
+            catch (Exception)
+            {
+                return lstEvaluadores;
+            }
+        }
+
         #endregion
     }
 }
