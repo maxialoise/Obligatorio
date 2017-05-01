728c23634bc282806189b7771d03a62e2e718111
Maxi Aloise <maxialoise@gmail.com>
2017-05-01 13:06:48 -0300

    Add Asignar evaluadores

---
+++ NegocioWeb/AsignarEvaluadores.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterEmpresa.Master" AutoEventWireup="true" CodeBehind="AsignarEvaluadores.aspx.cs" Inherits="NegocioWeb.AsignarEvaluadores" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Evaluadores: "></asp:Label>
+    <asp:DropDownList ID="ddlEvaluadores" runat="server"></asp:DropDownList>
+    <b>
+        <asp:Label ID="lblAvisoEval" runat="server"></asp:Label>
+    </b>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Emprendimientos: "></asp:Label>
+    <asp:DropDownList ID="ddlEmprendimientos" runat="server"></asp:DropDownList>
+    <b>
+        <asp:Label ID="lblAvisoEmpren" runat="server"></asp:Label>
+    </b>
+    <br />
+    <asp:Button ID="btnIngresar" runat="server" Text="Asignar" OnClick="btnIngresar_Click" CssClass="btn btn-primary" />
+    <br />
+    <asp:Label ID="lblError" runat="server"></asp:Label>
+</asp:Content>
---
+++ NegocioWeb/AsignarEvaluadores.aspx.cs
@@ -0,0 +1,56 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NegocioWeb
+{
+    public partial class AsignarEvaluadores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario usu = Session["usuario"] as Usuario;
+
+                if (usu == null || usu.Rol != "Admin")
+                {
+                    Response.Redirect("Login.aspx");
+                }
+
+                if (!IsPostBack)
+                {
+                    BindData();
+                }
+            }
+            catch (Exception)
+            {
+                lblError.Text = "Error en carga de pagina";
+            }
+        }
+
+        private void BindData()
+        {
+            List<Evaluador> evaluadores = Evaluador.ObtenerEvaluadores();
+
+            if (evaluadores.Count <= 0)
+            {
+                lblAvisoEval.Text = "No hay Evaluadores en el sistema";
+            }
+            ddlEvaluadores.DataSource = evaluadores;
+            ddlEvaluadores.DataTextField = "Nombre";
+            ddlEvaluadores.DataValueField = "Id";
+            ddlEvaluadores.DataBind();
+
+            //List<Emprendimiento> emprendimientos = Emprendimiento.ObtenerEmprendimientos();
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
