fee572dde3929e5aa9412abda1f381dd3a1f3138
francisco arriola <franarrio@gmail.com>
2017-05-01 14:20:03 -0300

    .

--- Obligatorio/Evaluacion.cs
+++ Obligatorio/Evaluacion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +29,51 @@ namespace Dominio
 
         #region Metodos
 
+        public bool AltaEvaluacion(int idEmprendimiento, out string message)
+        {
+            bool result = false;
+            message = string.Empty;
 
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(@"Server=PC-102717\FARRIOLA; Database = Emprendimientos;Integrated Security=SSPI"))
+                {
+                    SqlCommand cmd = new SqlCommand("Alta_Evaluacion", cnn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@evaluador", this.Evaluador.Id);
+                    cmd.Parameters.AddWithValue("@idEmprendimiento", idEmprendimiento);
+                    cnn.Open();
+
+                    int res = cmd.ExecuteNonQuery();
+
+                    if (res == -1)
+                    {
+                        message = "Ya se encuentra asignado el evaluador a el emprendimiento seleccionado";
+                    }
+                    else if (res == -2)
+                    {
+                        message = "El emprendimiento seleccionado ya cuenta con 3 evaluadores";
+                    }
+                    else if (res > 0)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        message = "Error inesperado";
+                    }
+
+                    cmd.Dispose();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
 
         #endregion
     }
